b09aee7e36512c4dfe73c23fc98dc202902b78b0
oleksandr.chebernin <lfiecbr1>
2017-12-05 23:21:17 +0100

    destructional assignments

--- app/index.js
+++ app/index.js
@@ -1,1 +1,31 @@
-console.log('Hello world from webpack with automatic update 122!');
+function run() {
+
+    let b = 2;
+    console.log(b);
+
+    {
+        let b = 3;
+        console.log(b);
+    }
+
+    let c = 'hello';
+    let d = `${c} world`;
+    let arr = [1, 2, 3, 4, 5];
+    let data = [...arr];
+    console.log(data);
+    let a = [2,4,6];
+    let z = [1,2,...a,56];
+    console.log(d);
+    console.log(z);
+
+    const fellowship = ["Frodo", "Gandalf", "Aragorn", "Legolas", "Gimli"];
+    let [hobbit, ...others] = fellowship;
+    console.log(hobbit, ...others);
+
+
+    let wizard = {magical : true, power :9}
+    let {power, magical} = wizard;
+    console.log(magical, power);
+}
+
+run();
